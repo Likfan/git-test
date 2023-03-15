853cbd459f9284f2df07e8536e020d8cdbfaa775
Layne <Layne@lkf.com>
2023-03-15 13:28:00 +0800

    hot-fix first commit

--- src/main/java/com/lkf/GitTest.java
+++ src/main/java/com/lkf/GitTest.java
@@ -3,7 +3,11 @@ package com.lkf;
 public class GitTest {
 
     public static void main(String[] args) {
-        System.out.println("hello git!");
+        System.out.println("hello git1!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
+        System.out.println("hello git4!");
+        System.out.println("hello git5!");
     }
 
 }
